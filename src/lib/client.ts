5b8126cb99117bea286aced079292d2e4c26d6b5
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-02-27 20:05:21 +0300

    feat: create client interface for graphql

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,9 +1,10 @@
 export let gql = String.raw;
 
-const url = "127.0.0.1:8080/graphql"
+const query = "127.0.0.1:8080/graphql"
+const ws = "127.0.0.1:8080/ws"
 
 export const gateway = async (query: string, variables?: any, token?: string) => {
-    const response = await fetch(url, {
+    const response = await fetch(query, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
