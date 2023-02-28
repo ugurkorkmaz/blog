fa4fb9d8cc49e3c99c09750ef0872c18262c09b3
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-03-01 01:21:17 +0300

    chore: padding -> 4

--- src/routes/[slug]_[id]/+page.svelte
+++ src/routes/[slug]_[id]/+page.svelte
@@ -9,7 +9,7 @@
   <meta name="googlebot" content="noindex" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css" integrity="sha512-Ya9H+OPj8NgcQk34nCrbehaA0atbzGdZCI2uCbqVRELgnlrh8vQ2INMnkadVMSniC54HChLIh5htabVuKJww8g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </svelte:head>
-<div class="markdown-body  p-1 md:p-8">
+<div class="markdown-body  p-4 md:p-8">
   {@html data.bodyHTML}
 </div>
 
