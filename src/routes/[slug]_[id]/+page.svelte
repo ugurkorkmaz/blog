4d1766634520dd9b1602bb554b0a71ccba08133e
ugurkorkmaz <korkmaz.uu@gmail.com>
2023-03-01 01:11:48 +0300

    chore: daisyui implement and hard update

--- src/routes/[slug]_[id]/+page.svelte
+++ src/routes/[slug]_[id]/+page.svelte
@@ -5,11 +5,8 @@
 
 <svelte:head>
   <title>Uğur Korkmaz</title>
-  <meta name="description" content="{data.title}" />
-  <meta
-    name="keywords"
-    content="golang,php,javascript,typescript,graphql,symfony,gqlgen,entgo,docker,tailwindcss,html,css,js"
-  />
+  <meta name="robots" content="noindex" />
+  <meta name="googlebot" content="noindex" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown.min.css" integrity="sha512-Ya9H+OPj8NgcQk34nCrbehaA0atbzGdZCI2uCbqVRELgnlrh8vQ2INMnkadVMSniC54HChLIh5htabVuKJww8g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </svelte:head>
 <div class="markdown-body p-8">
